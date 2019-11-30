413a211098f0b8cf5a38cbe9b39d3040c3715b96
Preston <preston@prestonkemp.com>
2019-11-29 20:52:45 -0600

    Initial Commit

---
+++ LDAPCertificateRetrival/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LDAPCertificateRetrival
+//
+//  Created by Preston Kemp on 11/29/19.
+//  Copyright © 2019 Preston Kemp. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
